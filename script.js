0baf23d6bd67a08c4580247bb6c237f268f43e28
Brandon Alvarez <balvarez325@gmail.com>
2022-09-29 09:44:43 -0400

    Slideshow functioning automatically

--- script.js
+++ script.js
@@ -2,18 +2,19 @@
 
 const url = "https://acnhapi.com/v1/villagers/";
 
-const characterContainer = document.querySelector(".char-container");
+const villagerContainer = document.querySelector(".villager-container");
+const gallery = document.querySelector(".gallery");
 
 function displayUI(dataObj) {
   console.log(dataObj);
   for (const villager in dataObj) {
-    console.log(villager);
-    console.log(dataObj[villager]["catch-phrase"]);
-    console.log(dataObj[villager]["image_uri"]);
-    console.log(dataObj[villager]["name"]["name-USen"]);
+    // console.log(villager);
+    // console.log(dataObj[villager]["catch-phrase"]);
+    // console.log(dataObj[villager]["image_uri"]);
+    // console.log(dataObj[villager]["name"]["name-USen"]);
 
     let htmlTemplate = `
-    <div class="card">
+    <li class="villager-card">
       <h2>${dataObj[villager]["name"]["name-USen"]}</h2>
       <div class="gen-info">
         <p>Species: ${dataObj[villager]["species"]}</p>
@@ -23,10 +24,10 @@ function displayUI(dataObj) {
 
       <img src="${dataObj[villager]["image_uri"]}" alt="${dataObj[villager]["name"]["name-USen"]} fish" width="250" height="250">
 
-    </div>
+    </li>
   `;
 
-    characterContainer.insertAdjacentHTML("afterend", htmlTemplate);
+    gallery.insertAdjacentHTML("afterbegin", htmlTemplate);
   }
 }
 
@@ -34,6 +35,7 @@ async function fetchChars() {
   let response = await axios(url);
   // console.log(response.data);
   displayUI(response.data);
+  setSlideshow();
 }
 
 fetchChars();
@@ -60,3 +62,46 @@ fetchChars();
 
 //   characterContainer.insertAdjacentHTML("afterend", htmlTemplate);
 // }
+//use char container
+// setTimeout(() => {
+function setSlideshow() {
+  const galleryItems = villagerContainer.querySelectorAll(".villager-card");
+  const slideCount = galleryItems.length;
+  console.log(slideCount);
+  const width = galleryItems[0].clientWidth;
+  console.log(width);
+  let currentSlide = 1;
+  function transitionSlide() {
+    if (currentSlide < slideCount) {
+      console.log("Called!");
+      gallery.style.transform = `translateX(-${width * currentSlide}px)`;
+      currentSlide += 1;
+    } else {
+      gallery.style.transform = `tranlsateX(0)`;
+      currentSlide = 1;
+    }
+  }
+  let setInter = setInterval(transitionSlide, 5000);
+  setInter;
+}
+// }, 3500);
+
+// const galleryItems = villagerContainer.querySelectorAll(".villager-card");
+// const slideCount = galleryItems.length;
+// console.log(slideCount);
+// const width = galleryItems[0].clientWidth;
+// console.log(width);
+// let currentSlide = 1;
+// function transitionSlide() {
+//   if (currentSlide < slideCount) {
+//     console.log("Called!");
+//     gallery.style.transform = `translateX(-${width * currentSlide}px)`;
+//     currentSlide += 1;
+//   } else {
+//     gallery.style.transform = `tranlsateX(0)`;
+//     currentSlide = 1;
+//   }
+// }
+
+// let setInter = setInterval(transitionSlide, 1000);
+// setInter;
